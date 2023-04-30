499aa3a136f8d60b5577443d8e975138a36d37e4
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2023-04-30 10:22:44 +0200

    Optimisation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <img src="https://github.com/LeGeRyChEeSe/EVApy/blob/main/images/eva_logo.jpg?raw=true" align="center" height=205 alt="EVApy" />
 </p>
 <p align="center">
-<img src='https://visitor-badge.laobi.icu/badge?page_id=LeGeRyChEeSe.EVApy'>
+<img src='https://visitor-badge.laobi.icu/badge?page_id=LeGeRyChEeSe.EVApy', alt='Visitors'/>
 <a href="https://github.com/LeGeRyChEeSe/EVApy/stargazers">
 <img src="https://img.shields.io/github/stars/LeGeRyChEeSe/EVApy" alt="Stars"/>
 </a>
@@ -19,6 +19,7 @@ There is also some other basic features like getting booking sessions from any E
 
 ## Table of Contents
 - [Installation](#installation)
+- [Script Example](#script-example)
 - [Contributing](#contributing)
 - [License](#license)
 
@@ -26,6 +27,51 @@ There is also some other basic features like getting booking sessions from any E
 
 ```python
 pip install EVApy
+```
+
+## Script Example
+
+- Here is an example of a simple script using EVA API
+
+```python
+import asyncio
+import EVA.EVA as eva
+
+async def main():
+    # Get stats for a given player with it's discriminant
+    # Ex. 'EVA_USERNAME#12345'
+    player_stats = await eva.getStats(username='EVA_USERNAME#12345')
+
+    # Get all EVA cities for getting more informations
+    # We need the ID of the city actually
+    cities = await eva.getCities()
+
+    # For example we get the ID of the first location in 'cities'.
+    # It is the location ID for Tours city.
+    location = await eva.getLocation(cities[0]['id']) # So location['id] = 15
+    
+    # So we can finally get Last Games of the player
+    # We use the 'userId' of the player located in 'player_stats['player']['userId']'
+    # And location ID from 'location['id']'
+    last_games = await eva.getLastGames(player_stats['player']['userId'], 1, location['id'], items_limit=5)
+
+    # We print every results to check values
+    # Once every requests are done before, you can skip this part
+    # And do whatever you want with these data.
+    print("Player stats: ", player_stats, "\n")
+    print("Last Games of this Player: ", last_games, "\n")
+    print("EVA Location: ", location, "\n")
+    print("EVA Cities: ", cities, "\n")
+
+asyncio.run(main())
+```
+
+- Or you can use it in CLI
+
+```python
+>>> import asyncio
+>>> import EVA.EVA as eva
+>>> print(asyncio.run(eva.getStats(username='EVA_USERNAME#12345')))
 ```
 
 ## Contributing
@@ -45,5 +91,6 @@ Any contributions you make are **greatly appreciated**.
 
 Distributed under the MIT License. See [LICENSE](https://github.com/LeGeRyChEeSe/EVApy/blob/main/LICENSE) for more information.
 
------
+----
+
 Author/Maintainer: [Garoh](https://github.com/LeGeRyChEeSe/) | Discord: GarohRL#4449
--- src/EVA/EVA.py
+++ src/EVA/EVA.py
@@ -1,6 +1,6 @@
 import datetime
 import typing
-import EVApy.EVAerrors as EVAerrors
+import EVA.EVAerrors as EVAerrors
 from gql import gql, Client
 from gql.transport.exceptions import TransportQueryError
 from gql.transport.aiohttp import AIOHTTPTransport
@@ -12,21 +12,39 @@ from gql.transport.aiohttp import AIOHTTPTransport
     
    ┌─────────────────────────────────────────────────────┐
    │ Here is an example of a simple script using EVA API │
-   ├─────────────────────────────────────────────────────┴────────────────┐
-   │ import asyncio                                                       │
-   │ import EVApy.EVApy as eva                                            │
-   │                                                                      │
-   │ async def main():                                                    │
-   │     stats = await eva.getStats(username='EVA_USERNAME#12345')        │
-   │     last_games = await eva.getLastGames(32123, 1, 15, items_limit=5) │
-   │     location = await eva.getLocation(15)                             │
-   │     cities = await eva.getCities()                                   │
-   │                                                                      │
-   │     print(stats, '\n', last_games, '\n', location, '\n', cities)     │
-   │                                                                      │
-   │ asyncio.run(main())                                                  │
-   │                                                                      │
-   └──────────────────────────────────────────────────────────────────────┘
+   ├─────────────────────────────────────────────────────┴───────────────────────────────────────────────────────┐
+   │ import asyncio                                                                                              │
+   │ import EVA.EVA as eva                                                                                       │
+   │                                                                                                             │
+   │ async def main():                                                                                           │
+   │     # Get stats for a given player with it's discriminant                                                   │
+   │     # Ex. 'EVA_USERNAME#12345'                                                                              │
+   │     player_stats = await eva.getStats(username='EVA_USERNAME#12345')                                        │
+   │                                                                                                             │
+   │     # Get all EVA cities for getting more informations                                                      │
+   │     # We need the ID of the city actually                                                                   │
+   │     cities = await eva.getCities()                                                                          │
+   │                                                                                                             │
+   │     # For example we get the ID of the first location in 'cities'.                                          │
+   │     # It is the location ID for Tours city.                                                                 │
+   │     location = await eva.getLocation(cities[0]['id']) # So location['id] = 15                               │
+   │                                                                                                             │
+   │     # So we can finally get Last Games of the player                                                        │
+   │     # We use the 'userId' of the player located in 'player_stats['player']['userId']'                       │
+   │     # And location ID from 'location['id']'                                                                 │
+   │     last_games = await eva.getLastGames(player_stats['player']['userId'], 1, location['id'], items_limit=5) │
+   │                                                                                                             │
+   │     # We print every results to check values                                                                │
+   │     # Once every requests are done before, you can skip this part                                           │
+   │     # And do whatever you want with these data.                                                             │
+   │     print("Player stats: ", player_stats, "\n")                                                             │
+   │     print("Last Games of this Player: ", last_games, "\n")                                                  │
+   │     print("EVA Location: ", location, "\n")                                                                 │
+   │     print("EVA Cities: ", cities, "\n")                                                                     │
+   │                                                                                                             │
+   │ asyncio.run(main())                                                                                         │
+   │                                                                                                             │
+   └─────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 
    ┌────────────────────────────────────┐
    │ Here are the useful API functions  │
@@ -96,14 +114,19 @@ def _send_query():
                 except TransportQueryError as e:
                     if e.errors:
                         if e.errors[0]["message"] == 'User profile is private':
-                            raise EVAerrors.UserIsPrivate() from None
+                            raise EVAerrors.UserIsPrivate(kwargs['username'] if 'username' in kwargs else None)
                         elif e.errors[0]["message"] == "User not found":
-                            raise EVAerrors.UserNotFound() from None
+                            raise EVAerrors.UserNotFound(kwargs['username'] if 'username' in kwargs else None)
                         else:
                             raise
                 except:
                     raise
                 else:
+                    if 'locations' in result:
+                        if 'nodes' in result['locations']:
+                            return result['locations']['nodes']
+                    elif 'location' in result:
+                        return result['location']
                     return result
         return inner
     return outer
